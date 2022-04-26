9ae70778cf599bc72832f1e2f7ff232a8d656c88
Seanpen <sean.pinote@usjr.edu.ph>
2022-04-26 14:55:36 +0800

    class weather (to be continued..)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_moment/simple_moment.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/model/weather_model.dart
@@ -0,0 +1,10 @@
+class Weather {
+  late String city;
+  late double temp;
+  late double wind;
+
+  Weather({required this.city, required this.temp, required this.wind});
+
+  
+
+}
